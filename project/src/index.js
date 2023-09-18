2ac45771ed08ec5ac26b181253e8ccb67922054a
fa-kohansal <farnaz_kohansal@yahoo.com>
2023-09-18 10:57:06 +0330

    test

--- project/src/index.js
+++ project/src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <div>
+      <h1>Hello-world</h1>
+    </div>
   </React.StrictMode>
 );
 
